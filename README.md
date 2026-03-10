15a76977fa28380b86f4c1f99fdca87dc5e00602
cnRachel <cnRachel@users.noreply.github.com>
2026-03-10 13:59:37 -0600

    added waveform image and finished test bench

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Targeted toward Digilent Basys3. Make sure to install the [board files](https://
 
 Built for Vivado 2024.2 on Windows 11.
 
+## Test Bench Waveform
+![Waveform from thunderbird_fsm_tb](ECE281Lab3Waveform.jpg)
--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -54,31 +54,98 @@ library ieee;
 entity thunderbird_fsm_tb is
 end thunderbird_fsm_tb;
 
-architecture test_bench of thunderbird_fsm_tb is 
+architecture behavior of thunderbird_fsm_tb is 
 	
-	component thunderbird_fsm is 
---	  port(
-		
---	  );
-	end component thunderbird_fsm;
+	component thunderbird_fsm
+	  port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+      );
+	end component;
 
 	-- test I/O signals
+	signal w_reset : std_logic := '0';
+	signal w_clk : std_logic := '0';
+	signal w_turn : std_logic_vector(1 downto 0) := "00"; -- for left and right signals
+	
+	signal w_thunderbird : std_logic_vector(5 downto 0) := "000000"; -- one-hot
 	
 	-- constants
-	
+	constant k_clk_period : time := 10 ns;
 	
 begin
 	-- PORT MAPS ----------------------------------------
+	uut: thunderbird_fsm port map (
+	       i_clk => w_clk,
+	       i_reset => w_reset,
+	       i_left => w_turn(1),
+	       i_right => w_turn(0),
+	       o_lights_L => w_thunderbird(5 downto 3),
+	       o_lights_R => w_thunderbird(2 downto 0)
 	
+	   );
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    clk_proc : process
+	begin
+		w_clk <= '0';
+        wait for k_clk_period/2;
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
 	
+	test_proc: process
+	begin
+	
+	   -- sequential timing		
+	   w_reset <= '1';
+	   wait for k_clk_period;
+		 assert w_thunderbird = "000000" report "bad reset" severity failure;
+		
+	   w_reset <= '0';
+	   wait for k_clk_period;
+	   
+	   -- left turn, LA on
+	   w_turn <= "10"; wait for k_clk_period;
+	       assert w_thunderbird = "001000" report "LA should be on" severity failure;
+	   -- LA and LB on
+	   wait for k_clk_period;
+	       assert w_thunderbird = "011000" report "LA and LB should be on" severity failure;
+	   -- LA, LB, and LC on
+	   wait for k_clk_period;
+	       assert w_thunderbird = "111000" report "LA, LB, and LC should be on" severity failure;
+	   -- Back to OFF
+       wait for k_clk_period;
+           assert w_thunderbird = "000000" report "no lights should be on" severity failure;
+           
+       -- right turn, RA on
+	   w_turn <= "01"; wait for k_clk_period;
+	       assert w_thunderbird = "000001" report "RA should be on" severity failure;
+	   -- RA and RB on
+	   wait for k_clk_period;
+	       assert w_thunderbird = "000011" report "RA and RB should be on" severity failure;
+	   -- RA, RB, and RC on
+	   wait for k_clk_period;
+	       assert w_thunderbird = "000111" report "RA, RB, and RC should be on" severity failure;
+	   --- Back to OFF
+       wait for k_clk_period;
+           assert w_thunderbird = "000000" report "no lights should be on" severity failure;
+	
+	   -- test hazards
+	   w_turn <= "11"; wait for k_clk_period;
+	       assert w_thunderbird = "111111" report "all lights should be on" severity failure;
+	   wait for k_clk_period;
+	       assert w_thunderbird = "000000" report "no lights should be on" severity failure;
+	   
+	   wait;
+	end process;
 	-----------------------------------------------------	
 	
-end test_bench;
+end;
